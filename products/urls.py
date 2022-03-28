4b0f04beda9e9f72a8c31d3eaf7f65810a99b9b4
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-03-28 10:06:52 +0000

    edit home page styling

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_bikes, name="all_bikes"),
+    path('', views.all_bikes, name="products"),
     path('<product_id>', views.bike_detail, name="bike_detail"),
 ]
