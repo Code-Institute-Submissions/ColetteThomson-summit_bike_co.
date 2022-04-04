1783201c85654314774d62b919c072671d8aea62
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-04-04 14:57:24 +0000

    add shopping bag preview, additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -40,26 +40,38 @@ def add_to_bag(request, item_id):
             if size in bag[item_id]['items_by_size'].keys():
                 # increment quantity for chosen size
                 bag[item_id]['items_by_size'][size] += quantity
+                # confirmation message to user (toasts)
+                messages.success(request,
+                                 f'Updated size {size.upper()} {product.bike_model} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 # set item to chosen quantity as is a new size for that item
                 bag[item_id]['items_by_size'][size] = quantity
+                # confirmation message to user (toasts)
+                messages.success(request,
+                                 f'Added size {size.upper()} {product.bike_model} to your bag')
         else:
             """
             if items not already in bag, add them using dictionary,
             (for multiple items with same id, but different sizes)
             """
             bag[item_id] = {'items_by_size': {size: quantity}}
+            # confirmation message to user (toasts)
+            messages.success(request, f'Added size {size.upper()} {product.bike_model} to your bag')
 
     else:
         # if item has no size
         if item_id in list(bag.keys()):
             # increment its quantity
             bag[item_id] += quantity
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Updated {product.bike_model} quantity to {bag[item_id]}')
         else:
             # if no bag, create one with item and quantity
             bag[item_id] = quantity
-            # confirmation message to user for product added to bag
-            messages.success(request, f'Added {product.bike_model} to your bag')
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Added {product.bike_model} to your bag')
 
     # overwrite session with updated variable
     request.session['bag'] = bag
@@ -69,6 +81,7 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """ edit quantity of specified product to specified amount """
+    product = get_object_or_404(Product, pk=item_id)
     # obtain quantity required (convert to integer from string)
     quantity = int(request.POST.get('quantity'))
     # set size
@@ -84,17 +97,29 @@ def adjust_bag(request, item_id):
         if quantity > 0:
             # drill into 'items-by-size' dictionary and update item's quantity
             bag[item_id]['items_by_size'][size] = quantity
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Updated size {size.upper()} {product.bike_model} quantity to {bag[item_id]["items_by_size"][size]}')
+
         else:
             # remove item if quantity submitted is zero
             del[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Removed size {size.upper()} {product.bike_model} from your bag')
     else:
         # if there's no size remove item
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request,
+                             f'Updated {product.bike_model} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Removed {product.bike_model} from your bag')
 
     # overwrite session with updated variable
     request.session['bag'] = bag
@@ -107,6 +132,7 @@ def remove_from_bag(request, item_id):
     items directly without setting quantity to zero"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -116,17 +142,26 @@ def remove_from_bag(request, item_id):
         if size:
             # remove only specific size requested from dictionary
             del bag[item_id]['items_by_size'][size]
-            # if size is only item in dictionary, delete item id as dictionary is now empty
+            # if size only item in dictionary, delete item id
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Removed size {size.upper()} {product.bike_model} from your bag')
         else:
             # if there's no size, remove (pop) from bag
             bag.pop(item_id)
+            # confirmation message to user (toasts)
+            messages.success(request,
+                             f'Removed {product.bike_model} from your bag')
 
         # overwrite session with updated variable
         request.session['bag'] = bag
         # imply item was successfully removed
         return HttpResponse(status=200)
+
     # return error to template
     except Exception as e:
+        # confirmation message to user (toasts)
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
