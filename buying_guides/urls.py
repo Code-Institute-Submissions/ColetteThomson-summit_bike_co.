5a730d59088464c8f384c1f41b5c6136d91e1ba8
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-05-03 14:52:56 +0000

    add content for mtb_sizing page

--- buying_guides/urls.py
+++ buying_guides/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.what_is_a_mtb, name="what_is_a_mtb"),
-    path('', views.mtb_sizing, name="mtb_sizing"),
+    path('mtb_sizing', views.mtb_sizing, name="mtb_sizing"),
 ]
