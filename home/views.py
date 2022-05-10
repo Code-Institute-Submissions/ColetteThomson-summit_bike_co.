2dec54d9083a5a55a28ce7b204c6e0e281e32731
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-05-10 11:18:31 +0000

    style home page and header

--- home/views.py
+++ home/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     """ return home page """
     return render(request, 'home/index.html')
+
+
+def contact_us(request):
+    """ return 'contact_us' page """
+    return render(request, 'info/contact_us.html')
+
+
+def what_is_a_mtb(request):
+    """ return 'what is a mountain bike?' page """
+    return render(request, 'buying_guides/what_is_a_mtb.html')
