ef1a6b9f9e6cb0bc76f9519b0e7a1a32dbc4d2c4
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-05-11 14:19:59 +0000

    style bike_detail page

--- summit_bike_co/settings.py
+++ summit_bike_co/settings.py
@@ -206,6 +206,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # media files (images)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
